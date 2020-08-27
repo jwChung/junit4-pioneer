c477ee39277dec9232413675033d10d3a3349143
Jin-Wook Chung <jwchung@hotmail.com>
2020-08-28 02:38:08 +0900

    라이브러리 지속 배포를 위한 gradle 및 workflows 작성
    다음과 같이 배포에 필요한 비밀 정보는 GitHub의 Secrets에 담아 두었다.
    특별히 secretKeyRingFile 파일은 base64 인코딩하여 저장했다.
    - sonatypeUsername
    - sonatypePassword
    - signing.keyId
    - signing.password
    - signing.secretKeyRingFile
    
    라이브러리 배포 과정에서 위 값을 gradle.propeties 파일에 설정되어 사용된다. 현재
    라이브러리는 staging 상태로 배포되며 close, release 단계는 이후에 자동화할 수
    있을지 검토한다.
    
    현재 라이브러리를 배포하는 workflows는 GitHub에서 release를 생성했을 때 트리거된다.
    
    related to: #16

--- src/main/java/com/github/jwchung/junit4pioneer/Repeat.java
+++ src/main/java/com/github/jwchung/junit4pioneer/Repeat.java
@@ -12,10 +12,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface Repeat {
-
-
     /**
-     * Gets repeat times.
+     * @return repeat times
      */
     int value();
 }
